de3b721198dddc662dbbafb6f2804ce95acc3869
Dzmitry Yarashevich <dimon_yarik@mail.ru>
2023-11-22 15:57:54 +0300

    PRIOR-91234: Append string to console 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world2");
         System.out.println("hello world122");
+        System.out.println("hello world124");
     }
 }
